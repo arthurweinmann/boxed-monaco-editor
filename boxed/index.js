9fc36b0b42d9e91c4d447699b89408e4f75a6f0d
Arthur Weinmann <arthur.weinmann@gmail.com>
2023-07-09 00:08:40 -0500

    try with the official example

--- boxed/index.js
+++ boxed/index.js
@@ -2,11 +2,13 @@ import * as monaco from 'monaco-editor';
 // or import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
 // if shipping only a subset of the features & languages is desired
 
-export function getMonacoEditor() {
+// Webpack tree shaking side effect unused variable thingy removes this or a variable declaration not used
+// TODO: figure it out
+function getMonacoEditor() {
 	return monaco.editor;
 }
 
-// monaco.editor.create(document.getElementById('container'), {
-// 	value: 'console.log("Hello, world")',
-// 	language: 'javascript'
-// });
+monaco.editor.create(document.getElementById('container'), {
+	value: 'console.log("Hello, world")',
+	language: 'javascript'
+});
