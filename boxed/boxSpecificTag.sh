be0e28044b712faf4677e3ed5df29a113f44283d
Arthur Weinmann <arthur.weinmann@gmail.com>
2023-07-10 13:57:43 -0500

    fix varname in boxSpecificTag.sh

--- boxed/boxSpecificTag.sh
+++ boxed/boxSpecificTag.sh
@@ -4,10 +4,13 @@ set -e
 srcTag=$1
 targetTag=$2
 
-git checkout -b $srcTag_branch tags/$srcTag
-git push origin $srcTag_branch
+tempbranch=$srcTag
+tempbranch+="_branch"
+
+git checkout -b $tempbranch tags/$srcTag
+git push origin $tempbranch
 git tag $targetTag
-git push origin --tags 
+git push origin --tags
 git checkout main
-git branch -d $srcTag_branch
-git origin --delete $srcTag_branch
+git branch -d $tempbranch
+git origin --delete $tempbranch
