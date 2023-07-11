0ab2a9dc41027a2be9fd7386bd8391bda42e620c
Arthur Weinmann <arthur.weinmann@gmail.com>
2023-07-11 15:16:26 -0500

    automatically determine url of javascript file to import the web workers from the right location

--- boxed/index.js
+++ boxed/index.js
@@ -4,6 +4,34 @@ import * as monaco from 'monaco-editor';
 
 self.MonacoEnvironment = {
 	getWorkerUrl: function (moduleId, label) {
+		let script = document.getElementById("boxedMonacoScript");
+		if (script !== undefined) {
+			let src = script.getAttribute('src');
+			let spl = src.split("/");
+			spl = spl.pop;
+			if (spl !== undefined) {
+				src = spl.join("/");
+
+				if (src.endsWith('/')) {
+					src = src.slice(0, -1);
+				}
+
+				if (label === 'json') {
+					return src + '/json.worker.boxedmonaco.js';
+				}
+				if (label === 'css' || label === 'scss' || label === 'less') {
+					return src + '/css.worker.boxedmonaco.js';
+				}
+				if (label === 'html' || label === 'handlebars' || label === 'razor') {
+					return src + '/html.worker.boxedmonaco.js';
+				}
+				if (label === 'typescript' || label === 'javascript') {
+					return src + '/ts.worker.boxedmonaco.js';
+				}
+				return src + '/editor.worker.boxedmonaco.js';
+			}
+		}
+
 		if (label === 'json') {
 			return './json.worker.boxedmonaco.js';
 		}
